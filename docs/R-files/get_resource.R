5a5b2d106969b11f9ec96c85ae8573838a5f3291
Del'riaan <121366351+delriaan@users.noreply.github.com>
2024-07-22 23:19:20 -0400

    Adding new file

---
+++ docs/R-files/get_resource.R
@@ -0,0 +1,21 @@
+get_resource <- (\(){
+  #' Get an R Script
+  #'
+  #' @param url The URL of the hosted R script. The \emph{.R} extension is optional and will be appended automatically.
+  #'
+  #' @return an unevaluated expression containing the contents of the matched file (exact match excluding the file extension).
+  f <- \(url){
+    url <- rlang::enexpr(url) |> as.character()
+    url <- glue::glue("https://delriaan.github.io/R-files/{url}{if (!grepl(\"[.]R$\", url)) \".R\"}")
+    req <- httr2::request(url)
+    httr2::req_perform(req) |>
+      httr2::resp_body_string() |>
+      sprintf(fmt = "{%s}") |>
+      str2lang()
+  }
+
+  purrr::possibly(
+    purrr::slowly(f, rate = purrr::rate_delay(1))
+    , otherwise = NULL
+    )
+})()
