8302a31bf4d4edd1055119f51d1c45cd28d6e319
Chionesu <sapienti.Dei@outlook.com>
2024-08-15 12:41:24 -0400

    Periodic Updates to Source Files

--- docs/R-files/get_resource.R
+++ docs/R-files/get_resource.R
@@ -4,18 +4,15 @@ get_resource <- (\(){
   #' @param url The URL of the hosted R script. The \emph{.R} extension is optional and will be appended automatically.
   #'
   #' @return an unevaluated expression containing the contents of the matched file (exact match excluding the file extension).
-  f <- \(url){
-    url <- rlang::enexpr(url) |> as.character()
-    url <- glue::glue("https://delriaan.github.io/R-files/{url}{if (!grepl(\"[.]R$\", url)) \".R\"}")
-    req <- httr2::request(url)
-    httr2::req_perform(req) |>
-      httr2::resp_body_string() |>
-      sprintf(fmt = "{%s}") |>
-      str2lang()
-  }
+  f <- \(url, root = "https://delriaan.github.io/R-files"){
+      url <- rlang::enexpr(url) |> as.character()
+      url <- glue::glue("{root}/{url}{if (!grepl(\"[.]R$\", url)) \".R\"}")
+      req <- httr2::request(url)
+      httr2::req_perform(req) |>
+        httr2::resp_body_string() |>
+        sprintf(fmt = "{%s}") |>
+        str2lang()
+    }
 
-  purrr::possibly(
-    purrr::slowly(f, rate = purrr::rate_delay(1))
-    , otherwise = NULL
-    )
+  purrr::possibly(purrr::slowly(f, rate = purrr::rate_delay(1)), otherwise = NULL)
 })()
