f08e319c2d2eb68c45364be291f3b90d8c8cbc63
Chionesu <sapienti.Dei@outlook.com>
2024-11-18 10:28:01 -0500

    Function Updates

--- docs/R-files/calc_rx_adherence.R
+++ docs/R-files/calc_rx_adherence.R
@@ -1,44 +1,60 @@
-# Adherence calculation methods:
-calc_adherence <- \(data, measure = c("MPR", "2", "CMA", "CMG", "CSA", "CSG", "7")){
+if (!exists("get_resource", envir = rlang::caller_env())){
+  Sys.getenv("GIT_REPOS") |> file.path("resources/R/get_resource.R") |> source()
+}
+
+if (!exists("get_user_agent")){
+  Sys.getenv("GIT_REPOS") |> file.path("resources/R/user_agents.R") |> source()
+}
+ 
+calc_adherence <- \(measure = c("MPR", "CMA", "CMG", "CSA", "CSG", "2", "7"), days_supply, service_date, .debug = FALSE){
   #' Medication Adherence Calculator
   #' 
-  #' @param data The input data created from `project.qmd::COMBINE_RX_SW_PROFILE_DATA`
-  #' @param measure A measure identifier as found in global object \code{adherence_measures}
+  #' @param measure A measure identifer as found in global object \code{adherence_measures}
+  #' @param days_supply,service_date The days of supply and dates of fill, respectively: all arguments should be of the same length.
   #' 
   #' @return A vector of adherence values
+  #' 
+  #' @note Negative values may indicate a true lapse but may also stem from data partitioning and sequencing in the parent context.
+  #' @note Default values per measure are used when NA or infinite values are detected in the penultimate result.
   #' 
   #' @references
   #' Lam, Wai Yin, Fresco, Paula, Medication Adherence Measures: An Overview, BioMed Research International, 2015, 217047, 12 pages, 2015. https://doi.org/10.1155/2015/217047
   measure <- match.arg(measure)
 
-  eqns <- rlang::quos(
-      MPR = purrr::modify_if(1 - fill_gap/days_supply, \(x) is.na(x)|is.infinite(x), ~NA)
+  assertive::assert_all_are_same_length(days_supply, service_date)
+
+  dT <- c(days_supply[1], diff(as.numeric(service_date)))
+  fill_gap <- dT - days_supply
+
+  # Evaluated equation definitions:
+  eqns <- list(
+      MPR = days_supply/(dT + fill_gap)
+      , CMA = cumsum(days_supply) / cumsum(dT + fill_gap)
+      , CMG = (\(x) ifelse(x < 0, 0, x))(cumsum(fill_gap)/cumsum(dT))
+      , CSA = days_supply/dT
+      , CSG = sum(fill_gap)/sum(dT)
       , `2` = NA
-      , CMA = NA
-      , CMG = NA
-      , CSA = NA
-      , CSG = NA
       , `7` = NA
       )
 
-  rlang::eval_tidy(eqns[[measure]], data = rlang::as_data_mask(data))
+  # Default values:
+  dflts <- c(MPR = 0, `2` = NA, CMA = 1, CMG = 0, CSA = 0, CSG = 0, `7` = NA)
+
+  if (.debug) browser()
+  
+  eqns[[measure]] |>
+    purrr::modify_if(\(x) is.na(x) | is.infinite(x), ~dflts[measure])
 }
 
-if (FALSE){
-# <snippet: med adherence data :: Medication Adherence (https://onlinelibrary.wiley.com/doi/10.1155/2015/217047)> ----
-  read.snippet(action = save, doc = NULL, med, adherence, data);
-  if (!exists("get_resource")){
-    Sys.getenv("GIT_REPOS") |> file.path("resources/R/get_resource.R") |> source()
-  }
-
-  if (!exists("get_user_agent")){
-    Sys.getenv("GIT_REPOS") |> file.path("resources/R/user_agents.R") |> source()
-  }
+get_adherence_defs <- \(browser_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"){
+  #' Retrieve Medication Adherence Definitions
+  #' 
+  #' @references https://onlinelibrary.wiley.com/doi/10.1155/2015/217047
+  #' 
   
   cookies <- "cookies.txt"
-  browser_agent <- get_user_agent()
   
-  med_adherence_web_resp <- "https://doi.org/10.1155/2015/217047" |>
+  med_adherence_web_resp <<- "https://doi.org/10.1155/2015/217047" |>
     httr2::request() |>
     httr2::req_user_agent(browser_agent) |>
     httr2::req_cookie_preserve(path = cookies) |>
@@ -64,11 +80,11 @@ if (FALSE){
     httr2::resp_body_html() 
   
   adherence_measures <- stringi::stri_pad_left(5:10, width = 4, pad = "0") |>
-    map(\(i){
+    purrr::map(\(i){
       med_adherence_web_resp |>
       rvest::html_element(glue::glue("#sec-{i}")) |>
       rvest::html_children() |>
-      map(rvest::html_text, trim = TRUE) |>
+      purrr::map(rvest::html_text, trim = TRUE) |>
       rlang::set_names(c("measures", "description"))
     }) |>
     data.table::rbindlist() |>
@@ -91,7 +107,7 @@ if (FALSE){
       , ~measures + meas_id + equation
       )
   
-  adherence_measures <- adherence_measures[tmp_obj, on = "meas_id"] |>
+  adherence_measures <<- adherence_measures[tmp_obj, on = "meas_id"] |>
     data.table::setcolorder("description", after = "equation") |>
     data.table::setattr("url", "https://doi.org/10.1155/2015/217047")
   
@@ -99,5 +115,4 @@ if (FALSE){
     assertive::assert_all_are_true()
   
   rm(tmp_obj)
-# </snippet>
 }
